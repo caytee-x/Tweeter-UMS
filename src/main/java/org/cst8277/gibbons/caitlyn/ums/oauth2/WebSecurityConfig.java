d54f6861536e5fc026c6fb20ea3cb6b40cf23746
Caitlyn <gibb0147@algonquinlive.com>
2022-12-08 13:15:35 -0500

    Cookie Working

--- src/main/java/org/cst8277/gibbons/caitlyn/ums/oauth2/WebSecurityConfig.java
+++ src/main/java/org/cst8277/gibbons/caitlyn/ums/oauth2/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 )
                 .exceptionHandling(exc -> exc
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
                 .logout(l -> l
                         .logoutSuccessUrl("/").permitAll()
--- src/main/java/org/cst8277/gibbons/caitlyn/ums/oauth2/oauth2Controller.java
+++ src/main/java/org/cst8277/gibbons/caitlyn/ums/oauth2/oauth2Controller.java
@@ -2,10 +2,15 @@ package org.cst8277.gibbons.caitlyn.ums.oauth2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class oauth2Controller {
@@ -14,7 +19,14 @@ public class oauth2Controller {
     private OAuth2AuthorizedClientService authorizedClientService;
 
     @GetMapping(value="/user")
-    public String sayHello(@AuthenticationPrincipal OAuth2User principal) {
-        return "Hello, " + principal.getAttribute("name");
+    public Map<String, Object> username(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
+
+    @GetMapping(value="/getAuthentication", produces="application/json")
+    public String getAuthentication(OAuth2AuthenticationToken auth) {
+        return  auth.toString();
+    }
+
+
 }
